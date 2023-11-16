e261709b03d4aa2fbc0e471d734dac6b45b8720d
Ahmd Rusdian <11211086@student.itk.ac.id>
2023-11-16 10:57:27 +0800

    change database name

--- config/database.js
+++ config/database.js
@@ -14,7 +14,7 @@ module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
+    database: `${DB_NAME}_development`,
     host: DB_HOST,
     port: DB_PORT,
     dialect: 'postgres',
